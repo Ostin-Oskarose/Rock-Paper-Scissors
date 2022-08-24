7efcfe5dc9ce3833d6e2a3a01f5fa8b44856aa41
Ostin-Oskarose <oskarcraft@gmail.com>
2022-08-24 11:22:10 +0200

    Implemented game

---
+++ game.py
@@ -0,0 +1,44 @@
+import random
+
+choices = ["rock", "paper", "scissors"]
+computerPoints = 0
+playerPoints = 0
+
+while True:
+
+    computerChoice = random.choice(choices)
+
+    playerChoice = input("Rock, Paper or Scissors?:").lower()
+
+    print("Computer picks " + computerChoice)
+
+    if playerChoice == computerChoice:
+        print("Draw!")
+    elif playerChoice == "rock":
+        if computerChoice == "paper":
+            print("Computer wins!")
+            computerPoints += 1
+        else:
+            print("Player wins!")
+            playerPoints += 1
+    elif playerChoice == "paper":
+        if computerChoice == "scissors":
+            print("Computer wins!")
+            computerPoints += 1
+        else:
+            print("Player wins!")
+            playerPoints += 1
+    elif playerChoice == "scissors":
+        if computerChoice == "rock":
+            print("Computer wins!")
+            computerPoints += 1
+        else:
+            print("Player wins!")
+            playerPoints += 1
+    elif playerChoice == "":
+        break
+    else:
+        print("Please pick a valid choice")
+
+    print("Player score:", playerPoints)
+    print("Computer score:", computerPoints)
